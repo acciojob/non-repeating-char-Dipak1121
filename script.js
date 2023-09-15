e33cf81038fd47957834cc97359224047fd21a80
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-09-15 14:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let mp = {};
+	for ( let i =0; i < str.length; i++ ){
+		if(mp[str[i]] != undefined ){
+			mp[str[i]] += 1;
+		}
+		else{
+			mp[str[i]] = 1;
+		}
+	}
+	for ( let i = 0; i < str.length; i++ ){
+		if(str[i] == 1){
+			return str[i];
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
